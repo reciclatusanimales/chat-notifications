8d3333f529e62d79135dff39e2196dc3a0aec845
Daniel Reyes Veas <danielreyesveas@gmail.com>
2021-04-12 14:31:39 +0200

    Merge

--- server.js
+++ server.js
@@ -17,25 +17,9 @@ const server = new ApolloServer({
 	},
 });
 
-<<<<<<< HEAD
-const app = express();
-app.use(cors());
-app.use(jsonParser);
-
-app.get("/api", (_, response) => response.send("Hello friend..."));
-
-server.applyMiddleware({ app });
-const httpServer = http.createServer(app);
-server.installSubscriptionHandlers(httpServer);
-
-httpServer.listen(PORT, () => {
-	console.log(`🚀 Server ready at http://localhost:${PORT}`);
-	console.log(`🚀 Subscriptions ready at http://localhost:${PORT}`);
-=======
 server.listen().then(({ url, subscriptionsUrl }) => {
 	console.log(`🚀 Server ready at ${url}`);
 	console.log(`🚀 Subscriptions ready at ${subscriptionsUrl}`);
->>>>>>> b310a3cfd58bdfd35a994837026cd1d149e1de0a
 
 	sequelize
 		.authenticate()
